5023a0571d42d8dc3b3567d8157175bb97a6a339
walkw00 <walker.ward1@gmail.com>
2016-07-28 22:58:58 -0600

    go

---
+++ js/init.js
@@ -0,0 +1,7 @@
+(function($){
+  $(function(){
+
+    $('.button-collapse').sideNav();
+      $('.modal-trigger').leanModal();
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
